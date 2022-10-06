63b9a3c3ba9f13fc728cd2be6df7b91f1ae3604a
dorohov-vo <dorohov-vo@itrev.ru>
2022-10-06 18:52:39 +0500

    task4 - parsing bus info

--- src/main/java/com/dorokhov/telegrambotm1/Service/TelegramBot.java
+++ src/main/java/com/dorokhov/telegrambotm1/Service/TelegramBot.java
@@ -2,8 +2,14 @@ package com.dorokhov.telegrambotm1.Service;
 
 import com.dorokhov.telegrambotm1.config.BotConfiguration;
 import com.dorokhov.telegrambotm1.repositories.UserRepository;
+import com.vdurmont.emoji.EmojiParser;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import com.dorokhov.telegrambotm1.model.User;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
@@ -16,9 +22,9 @@ import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @ComponentScan
 @Component
@@ -32,9 +38,13 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     static final String HELP_TEXT = "Этот бот позволяет принимать и отправлять сообщения под средством команд в меню и через ввод, " +
             "а так же сохранять и удалять информацию о запросах и пользователях. \n \n"
-            + "VERSION MARK2 \n\n"
+            + "VERSION MARK1 \n\n"
             + "Выберите /start - чтобы начать работу и получить приветвтвенное сообщение\n\n"
-            + "Выберите /help - чтобы получить эту справочную информацию ещё раз \n\n";
+            + "Выберите /help - чтобы получить эту справочную информацию ещё раз \n\n"
+            + "Выберите /mydata - получить информацию о своём аккаунте и дату регистрации \n\n"
+            + "Выберите /deletedata - удалить информацию о своём аккаунте и дату регистрации \n\n"
+            + "Выберите /bus24_balmoshnaya - чтобы узнать расписание автобуса 24 «ул.Памирская – Пл.Дружбы(по ул.Крупской)» Остановка: «Балмошная»\n\n"
+            + "Выберите /bus24_circus - чтобы узнать расписание автобуса 24 «ул.Памирская – Пл.Дружбы(по ул.Крупской)» Остановка: «Цирк» \n\n";
 
     public TelegramBot(BotConfiguration configuration) {
         this.configuration = configuration;
@@ -45,7 +55,10 @@ public class TelegramBot extends TelegramLongPollingBot {
         listOfCommands.add(new BotCommand("/mydata", "информация о пользователе, история запросов"));
         listOfCommands.add(new BotCommand("/deletedata", "удаление истории и информации"));
         listOfCommands.add(new BotCommand("/help", "информация о боте"));
-        listOfCommands.add(new BotCommand("/settings", "настройки"));
+        listOfCommands.add(new BotCommand("/bus24_balmoshnaya", "24 «Балмошная» «ул.Памирская – Пл.Дружбы»\n" +
+                "Остановка: «Балмошная» "));
+        listOfCommands.add(new BotCommand("/bus24_circus", "24 «Цирк» «ул.Памирская – Пл.Дружбы»\n " +
+                "Остановка: «Цирк» "));
 
         try {
             this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
@@ -73,13 +86,13 @@ public class TelegramBot extends TelegramLongPollingBot {
     /**
      * @param update check message from user and return response
      */
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
 
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-
 
             switch (messageText) {
 
@@ -100,11 +113,38 @@ public class TelegramBot extends TelegramLongPollingBot {
                     deleteUserInfo(update.getMessage());
                     break;
 
+                case "/bus24_balmoshnaya":
+                    String urlBalm = "http://www.m.gortransperm.ru/time-table/24/108302";
+                    getBusInfoPage(update.getMessage(), urlBalm);
+                    break;
+
+                case "/bus24_circus":
+                    String urlCircus = "http://www.m.gortransperm.ru/time-table/24/8100";
+                    getBusInfoPage(update.getMessage(), urlCircus);
+                    break;
+
                 default:
-                    sendMessage(chatId, "Прошу прощения, команда пока не поддерживается");
+                    String emojiAnswer = EmojiParser.parseToUnicode("\uD83D\uDE4A");
+                    sendMessage(chatId, "Прошу прощения, команда пока не поддерживается " + emojiAnswer
+                            + "\n\n Для получения списка команд выберите /help");
             }
         }
+    }
 
+    /**
+     * Method return all info by bus
+     */
+    public void getBusInfoPage(Message msg, String url) throws IOException {
+        Document busInfo = Jsoup.connect(url).get();
+
+        // Получаем содержимое указанного класса и элементы указанного тега
+        Elements liElements = busInfo.getElementsByClass("time-table").get(0).getElementsByTag("li");
+        String hour = "Час: ";
+        String minutes = "Минуты: ";
+        for (Element liElement : liElements) {
+            sendMessage(msg.getChatId(), hour + liElement.getElementsByClass("hour").text() +
+                    "  " + minutes + liElement.getElementsByClass("minute").text());
+        }
     }
 
     /**
@@ -146,7 +186,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             String answer = "Данные быди удалены";
             sendMessage(msg.getChatId(), answer);
             log.info("user chatId: " + msg.getChatId() + "is deleted");
-        }else {
+        } else {
             String answer = "Данные не найдены";
             sendMessage(msg.getChatId(), answer);
             log.info("user info not found");
@@ -162,6 +202,9 @@ public class TelegramBot extends TelegramLongPollingBot {
         log.info("Message send to user: " + userName);
     }
 
+    /**
+     * Method send message
+     */
     private void sendMessage(long chatId, String textMessage) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
